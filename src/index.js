daabe6ece0a752e4fb894b12961fb674904235eb
alerileri <ale.ortmed@gmail.com>
2024-07-03 17:27:08 -0600

    Add new button function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
+let button = document.getElementById('validatorButton');
+
+button.addEventListener('click', function(){
+    let cardNumber = document.getElementById('inputCardNumber').value;
+    alert('El input es ' + cardNumber);
+
+})
+
+
+
 console.log(validator);
